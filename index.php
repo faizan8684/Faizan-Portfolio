bdd8c452b0455b93b6961dff7599e2b79c5604fd
faizan8684 <faizan251298@gmail.com>
2020-10-27 10:27:25 +0530

    rename

---
+++ index.php
@@ -0,0 +1,317 @@
+<!--
+
+=========================================================
+* Now UI Kit - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-kit
+* Copyright 2019 Creative Tim (http://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/now-ui-kit/blob/master/LICENSE.md)
+
+* Designed by www.invisionapp.com Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Faizan Akhtar
+  </title>
+  <meta name="title" content="Faizan Akhtar">
+    <meta name="description" content="A student with great visions for a collaborative society">
+    <meta name="author" content="Faizan Akhtar(faizanakhtar.com)">
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!-- CSS Files -->
+  <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="../assets/css/now-ui-kit.css?v=1.3.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="../assets/demo/demo.css" rel="stylesheet" />
+  <link href="/assets/css/myport.css" rel="stylesheet" />
+  <link rel="shortcut icon" type="image/jpg" href="/assets/icons/favicon.ico"/>
+
+<style>
+html{
+  scroll-behavior: smooth;
+}
+
+body,html{
+  height: 100%;
+  margin: 0;
+}
+
+.profile-page .gallery
+{
+  padding-bottom: 0px;
+}
+ </style>
+</head>
+
+<body class="profile-page sidebar-collapse">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent" color-on-scroll="50">
+    <div class="container">
+      
+      <div class="navbar-translate" style="font-size: large;">
+        <a class="navbar-brand" href="index.html" rel="tooltip" data-placement="bottom">
+          Faizan
+        </a>
+        <button class="navbar-collapsee navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-bar top-bar"></span>
+          <span class="navbar-toggler-bar middle-bar"></span>
+          <span class="navbar-toggler-bar bottom-bar"></span>
+        </button>
+      </div>
+      
+      <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="/assets/img/Faizan_Hololens.jpg">
+        <ul class="navbar-nav" style="font-size: large;">
+          <li class="nav-item">
+            <a class="nav-link" href="#AboutMe" >About Me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#Projects">Know More</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#Contact">Contact Me</a>
+          </li>
+          <li class="pl-3 nav-item mt-1" >
+           <a href="#"><i class="fa fa-arrow-up"> <img class="rounded-circle"  alt style="width: 2em;"></i> </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+  <style>
+  
+  .backg {
+    /* background: url(../assets/img/faizann.jpg) repeat scroll center center / cover;
+    height: 100vh;
+    width: 100%;
+     */
+     background-image: url("/assets/img/faizann.jpg");
+
+/* Full height */
+height: 100%; 
+
+/* Center and scale the image nicely */
+/* background-position: center;
+background-repeat: no-repeat;
+background-size: cover; */
+
+}
+</style>
+  <div class="wrapper" style="min-height: 100%;">
+    <div class="page-header clear-filter " filter-color="orange" >
+      <div class="page-header-image backg" data-parallax="true" style="height: 100%; 
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      ">
+      </div>
+      <div class="container" >
+        <div class="photo-container" >
+          <img src="/assets/img/Faizan_Hololens.jpg" alt="">
+        </div>
+        
+        <h3 class="mt-5" id="Name">Hi, I am Faizan Akhtar</h3>
+        <!-- <p class="category">a Student</p> -->
+        <script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
+		<script type="text/javascript">
+			var app = document.getElementById("Name"),
+				typewriter = new Typewriter(app, { loop: !0 });
+			typewriter
+				.typeString("Hi, I am Faizan Akhtar")
+				.pauseFor(3000)
+        .deleteAll(13)
+        .typeString("A student,")
+        .pauseFor(2000)
+        .deleteAll(9)
+        .typeString("Avid Learner,")
+        .pauseFor(2000)
+        .deleteAll(13)
+        .typeString("Community Enthusiast,")
+        .pauseFor(2000)
+        .deleteAll(21)
+        .typeString("Mixed Reality Evangelist")
+        .pauseFor(3000)
+        .deleteAll(24)
+        .typeString("Faizan Akhtar!")
+        .pauseFor(90000000)
+				.start();
+		</script>
+        
+        
+      </div>
+    </div>  
+    <div class="section">
+      <div class="container">
+        <div class="button-container" >
+          <a style="background-color: gainsboro;"  href="https://linktr.ee/FaizanSessions" class="mr-5 btn btn-default btn-round btn-lg btn-icon" rel="tooltip" title="My Sessions">
+            <svg class="svg-icon" enable-background="new 0 0 512 512" height="auto" viewBox="0 0 512 512" width="60%" xmlns="http://www.w3.org/2000/svg"><g><path d="m466.125 408.338c9.288-11.243 14.875-25.65 14.875-41.338 0-35.841-29.159-65-65-65-1.684 0-3.348.085-5 .211v-92.211h5c8.284 0 15-6.716 15-15s-6.716-15-15-15h-15v-45c0-3.978-1.58-7.793-4.394-10.606l-20-20c-5.857-5.858-15.355-5.858-21.213 0s-5.858 15.355 0 21.213l15.607 15.606v38.787h-20v-5c0-30.643-18.481-57.037-44.875-68.662 9.288-11.243 14.875-25.65 14.875-41.338 0-35.841-29.159-65-65-65s-65 29.159-65 65c0 15.688 5.587 30.095 14.875 41.338-26.394 11.625-44.875 38.019-44.875 68.662v5h-65c-8.284 0-15 6.716-15 15s6.716 15 15 15h5v92.211c-1.652-.126-3.316-.211-5-.211-35.841 0-65 29.159-65 65 0 15.688 5.587 30.095 14.875 41.338-26.394 11.625-44.875 38.019-44.875 68.662v20c0 8.284 6.716 15 15 15h480c8.284 0 15-6.716 15-15v-20c0-30.643-18.481-57.037-44.875-68.662zm-15.125-41.338c0 19.299-15.701 35-35 35s-35-15.701-35-35 15.701-35 35-35 35 15.701 35 35zm-195-337c19.299 0 35 15.701 35 35s-15.701 35-35 35-35-15.701-35-35 15.701-35 35-35zm-65 145c0-24.813 20.187-45 45-45h40c24.813 0 45 20.187 45 45v5h-130zm-60 35h250v102.271c-18.023 11.567-30 31.769-30 54.729 0 15.688 5.587 30.095 14.875 41.338-11.883 5.234-22.166 13.45-29.875 23.714-7.709-10.264-17.992-18.48-29.875-23.714 9.288-11.243 14.875-25.65 14.875-41.338 0-35.841-29.159-65-65-65s-65 29.159-65 65c0 15.688 5.587 30.095 14.875 41.338-11.883 5.234-22.166 13.45-29.875 23.714-7.709-10.264-17.992-18.48-29.875-23.714 9.288-11.243 14.875-25.65 14.875-41.338 0-22.96-11.977-43.162-30-54.729zm90 157c0-19.299 15.701-35 35-35s35 15.701 35 35-15.701 35-35 35-35-15.701-35-35zm-125-35c19.299 0 35 15.701 35 35s-15.701 35-35 35-35-15.701-35-35 15.701-35 35-35zm-65 145c0-24.813 20.187-45 45-45h40c24.813 0 45 20.187 45 45v5h-130zm160 0c0-24.813 20.187-45 45-45h40c24.813 0 45 20.187 45 45v5h-130zm290 5h-130v-5c0-24.813 20.187-45 45-45h40c24.813 0 45 20.187 45 45z"/></g></svg>
+          </a>
+          <a style="background-color: #0073b0;" href="https://www.linkedin.com/in/faizanakhtar8684/" class="mr-5 btn btn-default btn-round btn-lg btn-icon" rel="tooltip" title="Connect with me on LinkedIn">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a style="background-color: #1da1f2;" href="https://twitter.com/faizan8684" class="mr-5 btn btn-default btn-round btn-lg btn-icon" rel="tooltip" title="Follow me on Twitter">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a style="background: #f09433; 
+          background: -moz-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%); 
+          background: -webkit-linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%); 
+          background: linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%); 
+          filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f09433', endColorstr='#bc1888',GradientType=1 );" href="https://instagram.com/faizan.akhtar98" class="btn btn-default btn-round btn-lg btn-icon" rel="tooltip" title="Follow me on Instagram">
+            <i class="fab fa-instagram"></i>
+          </a>
+        </div>
+        <div class="mt-5 mr-2 ml-2 row" id="AboutMe">
+        <div class="col-md-8  mx-auto" style="border-radius: 5px; background-color: lavender;">
+        <h3 class="title text-center text-black" >About me</h3>
+        <h5 class="text-black description">A final year computer engineering student at <span><a style="font-weight: bolder;" href="http://www.mescoepune.org/">MESCOE</a></span>, Microsoft Learn Student Ambassador, India Community Outreach Manager at <a style="font-weight: bolder;" href="http://mrarvr.in/">VR AR MR</a>, and Ex-Mentor at <span><a style="font-weight: bolder;" href="https://www.dscmescoe.com/">DSC MESCOE</a></span> trying to grasp technical and leadership skills as much as possible.</h5>
+      </div>
+    </div>
+        
+    
+    <div id="Projects"class="row">
+          <div class="col-md-6 ml-auto mr-auto">
+            <h2 class="title text-center">Some of my Projects</h2>
+            <div class="nav-align-center">
+              <ul class="nav nav-pills nav-pills-primary nav-pills-just-icons" role="tablist">
+              </ul>
+            </div>
+          </div>
+          <!-- Tab panes -->
+          <div class="tab-content gallery mb-5">
+            <div class="tab-pane active" id="home" role="tabpanel">
+              <div class="col-md-10 ml-auto mr-auto">
+                <div class="row collections">
+                  <div class="col-md-6">
+                    <a href="http://www.mescoepune.org/alumni"><img src="/assets/img/alumniportal.jpg" alt="" class="img-raised"></a>
+                    <div class="mb-2 mt-2 caption text-center"><a href="http://www.mescoepune.org/alumni">MESCOE ALUMNI</a></div>
+                    <a href="https://www.faizanakhtar.com"><img src="/assets/img/portfolio.jpg" alt="" class="img-raised mt-1"></a>
+                    <div class="mb-2 caption text-center"><a href="https://www.faizanakhtar.com">MY PORTFOLIO</a></div>
+                  </div>
+                  <div class="col-md-6">
+                    <a href="https://1drv.ms/u/s!At5PgPLegaZVoGia6L2mGKSNflRF?e=MWcoMX"><img src="../assets/img/OC_MR.png" alt="" class="img-raised"></a>
+                    <div class="mb-2 caption text-center"><a href="https://1drv.ms/u/s!At5PgPLegaZVoGia6L2mGKSNflRF?e=MWcoMX">MIXED REALITY PROJECTS</a></div>
+                    <a href="https://www.drdalvi.com"><img src="assets/img/drdalvi.jpg" alt="" class="img-raised"></a>
+                  <div class="mb-2 caption text-center"><a href="https://www.drdalvi.com">DR DALVI</a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+        
+
+      <div class="m-0 pr-4 pb-4 justify-content-center" id="Contact" style="background-color: burlywood; border-radius: 5px;">
+          <div class="container">
+            <div class="row" >
+              <div class="col-lg-6 col-xs-12 mx-auto"> 
+                <h2 class="title mt-2 mb-3 " >Contact Me</h2>
+              </div> 
+    
+            </div>
+          </div>
+          <div class="container">
+            <div class="row container text-white">
+              
+              <div class="col-lg-6 col-xs-12 mt-2 ml-0 pl-0"> 
+               <div class="pt-2 pb-2 pr-1 mb-2" style="margin: 0px; background-color: #343a40;border-radius: 5px;">
+                <div class="ml-4  mt-1 " >
+                  <h4><i class="fa fa-location-arrow"></i><span class="mail text-white"> Pune, India</span></h4>
+                  <h4><i class="fa fa-phone" style="transform: rotate(90deg);"></i><span class=" mail text-white"> +91 9890677134</span></h4>
+                  <h4><i class="fa fa-envelope"></i><span class="ml-1 mail text-white"> akhtarf8684@gmail.com</span></h4>
+                </div>
+              </div> 
+            </div> 
+
+              <div class="col-lg-6 col-xs-12 pt-3 pl-3 mx-auto " style="background-color:darkcyan; border-radius: 5px;">
+                <script type="text/javascript">var submitted=false;</script>
+                <iframe name="hidden_iframe" id="hidden_iframe" style="display:none;" onload="if(submitted) {alert('Thank you! I will get back to you shortly. Please check your inbox.');window.location='/index.html';}"></iframe> 
+                <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSf9knrxXS93RCX1bP5Sm3ttYy78uyKYrdTSMxXTZKR0DfBGAw/formResponse" method="POST" target="hidden_iframe" onsubmit="return validateForm();">
+                  <div class="row ">
+                      <div class="col-lg-6 ">
+                        <div class="form-group">
+                          <input type="text" name="entry.1752164152" class="form-control " id="name" required placeholder="Your Full Name" style="background-color:whitesmoke; border-radius: 8px;"/>
+                        </div>
+                      </div>
+
+                      <div class="col-lg-6">
+                        <div class="form-group">
+                          <input type="email" name="entry.1949332219" class="form-control" id="email" required placeholder="Your Email" style="background-color:whitesmoke; border-radius: 8px;"/>
+                        </div>
+                      </div>
+
+                      <div class="col-lg-12">
+                        <div class="form-group">
+                          <textarea class="form-control" name="entry.738461940" rows="10" required placeholder="Query/Feedback" style="background-color:whitesmoke; border-radius: 2px;"></textarea>
+                        </div>
+                      </div>
+
+                      <div class="col-lg-12">
+                        <input type="submit" class="btn btn-success" value="Send message">
+                      </div>
+
+                    </div>
+                </form>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+    
+        
+        </div>
+      </div>
+    </div>
+
+    <footer class="footer footer-default ">
+      <div class="container">
+        <div class="text-center" id="copyright">
+          &copy;
+          <script>
+            document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+          </script>, Faizan Akhtar
+          
+        </div>
+      </div>
+    </footer>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+  <script src="../assets/js/plugins/bootstrap-switch.js"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="../assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+  <script src="../assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/now-ui-kit.js?v=1.3.0" type="text/javascript"></script>
+  <script>
+    function validateForm(){
+           submitted=true;
+        return true;
+      }
+    
+    </script>
+
+</body>
+
+</html>
